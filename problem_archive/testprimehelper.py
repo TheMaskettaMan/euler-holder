c2fcaa78e1efa9a5ebdf7e376796b53279420e1a
themaskettaman <themaskettaclan@gmail.com>
2018-05-25 02:57:09 -0700

    Policed personal testing file

--- problem_archive/testprimehelper.py
+++ problem_archive/testprimehelper.py
@@ -2,25 +2,27 @@
 
 # helps test prime functions.
 # Will eventually be updated to a better example of "unit tests"
+# also serves as a python training ground
 import primehelp
-
-print ("hey man")
 
 factors = primehelp.findprimefactors(14)
 
+
 if( (float(5)/2).is_integer() ) :
-	print ("what the fuck")
+	print ("should be failing")
 
 if( primehelp.findprimefactors(2.5)) :
-	print ("oh no you dont see me")
+	print ("shouldnt see me")
+
 if( primehelp.findprimefactors(7) ) :
-	print "its prime but can you see me?"
+	print "should be visible"
 	print primehelp.findprimefactors(7)
 
 print "checkprime tests"
 
 if (primehelp.check_prime(7)) :
 	print "you should see me"
+
 if (primehelp.check_prime(4)) :
 	print "you shouldnt see me"
 
@@ -28,5 +30,5 @@ if (primehelp.check_prime(9)) :
 	print "you shouldnt see me"
 
 if( factors ) :
-	print ("factors?")
+	print ("factors:")
 	print factors
